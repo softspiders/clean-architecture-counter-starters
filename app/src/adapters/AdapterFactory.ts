216e6b2c7de6f64fae207c6833e0a4209a829046
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-03-27 19:10:59 +0300

    refactor: simplification

--- app/src/adapters/AdapterFactory.ts
+++ app/src/adapters/AdapterFactory.ts
@@ -1,15 +1,10 @@
 import {CounterUseCaseOutRestGateway} from "./gateways";
-import {CounterUseCaseIn, CounterUseCaseOut} from "../domain/usecases";
+import {CounterUseCaseOut} from "../domain/usecases";
 import CounterUseCaseInReactPresenter from "./presenters/CounterUseCaseInReactPresenter";
-
-interface CounterPresenterProps {
-  functions: { handleIncrementClick: () => Promise<void> }
-  state: { counter: number }
-}
 
 export class AdapterFactory {
 
-  public static getReactPresenter(): (counterUseCaseIn: CounterUseCaseIn) => CounterPresenterProps {
+  public static getReactPresenter() {
     return CounterUseCaseInReactPresenter
   }
 
