1d1231ae8904ce4f6ba5415e35fe5007b91ea569
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-03-28 09:58:21 +0300

    refactor: simplification

--- app/src/adapters/AdapterFactory.ts
+++ app/src/adapters/AdapterFactory.ts
@@ -3,7 +3,6 @@ import { CounterUseCase } from '../domain/usecases'
 
 export class AdapterFactory {
   static getCounterUseCase(url: string): CounterUseCase {
-    const restGateway = new CounterUseCaseOutRestGateway(url)
-    return new CounterUseCase(restGateway)
+    return new CounterUseCase(new CounterUseCaseOutRestGateway(url))
   }
 }
