76a79b794ebba2e6c87e519aa832232227a8c4d3
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-03-29 08:04:36 +0300

    test: add coverage

--- app/src/domain/entities/__tests__/Counter.ts
+++ app/src/domain/entities/__tests__/Counter.ts
@@ -1,13 +1,16 @@
-import {Counter} from "../Counter";
+import { Counter } from '../Counter'
 
 describe('Counter', () => {
   it('Created with negative number should be the same', () => {
-    expect(new Counter(-3).counter).toBe(-3);
-  });
+    expect(new Counter(-3).counter).toBe(-3)
+  })
   it('Created with 0 should be 0', () => {
-    expect(new Counter(0).counter).toBe(0);
-  });
+    expect(new Counter(0).counter).toBe(0)
+  })
   it('Created with positive number should be the same', () => {
-    expect(new Counter(5).counter).toBe(5);
-  });
-});
+    expect(new Counter(5).counter).toBe(5)
+  })
+  // it('Created with positive number should be the same', () => {
+  //   expect(new Counter(5).counter1).toBe(5)
+  // })
+})
