f73232880a9978d30d40ac42b29701883ffceec0
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-04-02 11:57:25 +0300

    test: complete getCoutnter test

--- app/src/domain/usecases/__tests__/CounterUseCase.test.ts
+++ app/src/domain/usecases/__tests__/CounterUseCase.test.ts
@@ -1,7 +1,6 @@
 import { CounterUseCase, CounterUseCaseOut } from '../CounterUseCase'
 import { Counter } from '../../entities'
-import { CounterUseCaseOutRestGateway } from '../../../adapters/gateways'
-import {UseCaseFactory} from "../../../adapters";
+import { UseCaseFactory } from '../../../adapters'
 
 describe('CounterUseCase', () => {
   describe('CounterUseCaseIn', () => {
@@ -9,30 +8,17 @@ describe('CounterUseCase', () => {
       let useCaseFactory: UseCaseFactory
       beforeAll(() => {
         useCaseFactory = new UseCaseFactory('someUrl')
-      });
+      })
 
       it('should return right value', async () => {
-        const counterUseCaseOut = useCaseFactory.getCounterUseCaseOut()
-
-        // const promiseCounter1: Promise<Counter> = Promise.resolve(
-        //   new Counter(10)
-        // )
-        // const promiseCounter: Promise<Counter> = Promise.resolve(
-        //   new Counter(91)
-        // )
-
-
-        const counter10: Counter = new Counter(10)
-        const spy = jest
-          .spyOn(counterUseCaseOut, 'getCounter')
-          .mockResolvedValue(counter10)
-        //const counterUseCase = new CounterUseCase(counterUseCaseOutRestGateway)
-        const counterUseCaseIn = useCaseFactory.getCounterUseCaseIn()
-        await counterUseCaseIn.getCounter()
-        expect(spy).toBeCalled()
-        //expect(spy).toHaveReturnedWith(counter10)
-
-        spy.mockRestore()
+        const useCaseFactory = new UseCaseFactory('someUrl')
+        const COUNTER_VALUE = 99
+        jest
+          .spyOn(useCaseFactory.getCounterUseCaseOut(), 'getCounter')
+          .mockResolvedValue(new Counter(COUNTER_VALUE))
+        expect(await useCaseFactory.getCounterUseCaseIn().getCounter()).toBe(
+          COUNTER_VALUE
+        )
       })
     })
   })
