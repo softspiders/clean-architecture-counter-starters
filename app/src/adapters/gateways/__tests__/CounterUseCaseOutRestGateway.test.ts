cbba641e06f302037438d1b331c25fcc84a7f15c
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-04-04 09:07:15 +0300

    feat: add jest-fetch-mock

--- app/src/adapters/gateways/__tests__/CounterUseCaseOutRestGateway.test.ts
+++ app/src/adapters/gateways/__tests__/CounterUseCaseOutRestGateway.test.ts
@@ -1,10 +1,17 @@
-jest.mock('unfetch')
+import { enableFetchMocks } from 'jest-fetch-mock'
+enableFetchMocks()
 
-import fetch from 'unfetch'
+//import fetch from 'unfetch'
+
+jest.mock('unfetch')
 
 describe('CounterUseCaseOutRestGateway', () => {
   it('should ...', async () => {
-    fetch.mockReturnValue(Promise.resolve(new Response('4')))
+    // fetch.mockReturnValue(Promise.resolve(new Response('4')))
+    // fetch.mockResponse(() => callMyApi().then(res => ({body: res}))
+    // let response = await fetch();
 
+    fetch.resetMocks()
+    fetch.mockResponseOnce(JSON.stringify({ data: '12345' }))
   })
 })
