13fcf4cf573978c14ca45e9e5b55fd6a84737caf
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-04-11 08:26:28 +0300

    fead: add react-testing-library dependency

---
+++ app/src/ui/__tests__/Counter.test.ts
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, getByTestId } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Counter from '../Counter'
+
+xit('loads and displays greeting', () => {
+  // const { getByTestId } = render(Counter)
+  //
+  // const greetingTextNode = getByTestId('test-testid')
+  // expect(greetingTextNode).toHaveTextContent('Hi there')
+})
