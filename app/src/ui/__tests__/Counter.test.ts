fe4a2a01a205b8ebdd9d61659ee147816f93abff
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-04-11 16:56:14 +0300

    test: update test for Counter

--- app/src/ui/__tests__/Counter.test.ts
+++ app/src/ui/__tests__/Counter.test.ts
@@ -2,10 +2,13 @@ import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Counter, { CounterProps } from '../Counter'
 
-it('should display "Click" on button', () => {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  const { getByTestId } = render(Counter(<CounterProps>{}))
+describe('Counter()', () => {
+  it('should display "Click" on the button', () => {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const { getByTestId } = render(Counter(<CounterProps>{}))
 
-  const buttonTextNode = getByTestId('button-testid')
-  expect(buttonTextNode).toHaveTextContent('Click')
+    const buttonTextNode = getByTestId('button-testid')
+    expect(buttonTextNode).toHaveTextContent('Click')
+  })
 })
+
